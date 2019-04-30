37bd8190617d7e947a22074c18bac4d62a22e607
Amiya Behera <amiyatulu@gmail.com>
2019-04-30 21:27:17 +0530

    Election 2

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,9 +2,30 @@ pragma solidity >=0.4.22 <0.7.0;
 
 
 contract Election {
-    string public candidate;
 
+    // Model a candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    
+    
+    // Store candidate
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+
+     // Store candidate count
+    uint public candidatesCount;
+
+    
     constructor () public {
-        candidate = "Candidate 1";
+       addCandidate("Candidate 1");
+       addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
