9d2ced187003f574c98407d4a55bb7cdf992bb7b
Amiya Behera <amiyatulu@gmail.com>
2019-06-24 15:53:07 +0530

    Cast Votes

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -9,6 +9,8 @@ contract Election {
         string name;
         uint voteCount;
     }
+    // Storing accounts that have voted
+    mapping(address => bool) public voters;
 
     // Store candidate
     // Fetch Candidate
@@ -25,5 +27,18 @@ contract Election {
     function addCandidate(string memory _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+    function vote(uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender], "Sender not authorized");
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount, "Candidate is not valid");
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
     }
 }
